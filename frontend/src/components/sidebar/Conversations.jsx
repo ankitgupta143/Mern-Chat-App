9acb29977d38758a9c2e56c4a57bbb783d4f4404
Ankit Gupta <ag123@gmail.com>
2024-04-02 19:23:49 +0530

    deployment

--- frontend/src/components/sidebar/Conversations.jsx
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
 import Conversation from './Conversation'
+import useGetConersations from '../../hooks/useGetConersations';
+import { getRandomEmoji } from '../../utils/emojis';
+
 const Conversations = () => {
+  const {loading, conversations} =  useGetConersations();
   return (
+
     <div className='py-2 flex flex-col overflow-auto'>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
-      <Conversation/>
+      {conversations.map((conversation, idx) => (
+        <Conversation 
+          key={conversation._id}
+          conversation = {conversation}
+          emoji = {getRandomEmoji()}
+          lastIdx = {idx === conversations.length - 1}
+        />
+      ))}
+
+      {loading ? <span className='loading loading-spinner mx-auto'></span> : null}
     </div>
-  )
-}
+  );
+};
 
 export default Conversations
 
