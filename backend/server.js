be6e3c8567c42c58d278d7143d8bcb79fcf1177d
Ankit Gupta <ag123@gmail.com>
2024-04-08 12:36:00 +0530

    Updated Code

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,3 @@
-import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -11,8 +10,6 @@ import { app, server } from './socket/socket.js';
 
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
-
 dotenv.config();
 
 app.use(express.json());
@@ -22,14 +19,9 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")))
-
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
-})
-
-
-
+app.get( "/", (req, res) => {
+    res.send("Server is running");
+});
 
 
 server.listen(PORT, () => {
